0c5bb721861f3b05848ccc5b0f10ec76a3bd5a37
Sacha Bron <sacha.bron@copperflake.com>
2018-03-23 11:53:00 +0100

    Add notes about the architecture of this project

--- index.js
+++ index.js
@@ -4,25 +4,47 @@ const mpd = require('mpd')
 const cmd = mpd.cmd
 
 const client = mpd.connect({
-    port: 6600,
-    host: 'mpd.fixme.ch',
+	port: 6600,
+	host: 'mpd.fixme.ch',
 })
 
 client.on('ready', function() {
-    console.log("ready")
+	console.log("ready")
 })
 
 // Trigger: on playlist update, on volume update
 client.on('system', function(name) {
-    console.log("update", name)
+	console.log("update", name)
 })
 
 // Trigger: on play/pause
 // Trigger: on next song
 // Trigger: fast forward/backward
 client.on('system-player', function() {
-    client.sendCommand(cmd("status", []), function(err, msg) {
-        if (err) throw err
-        console.log(msg)
-    })
+	client.sendCommand(cmd("status", []), function(err, msgStr) {
+		if (err) throw err
+		console.log(msgStr)
+		var msg = mpd.parseKeyValueMessage(msgStr)
+		/*
+		volume: 75
+		repeat: 0
+		random: 0
+		single: 0
+		consume: 0
+		playlist: 4281
+		playlistlength: 148
+		xfade: 0
+		mixrampdb: 0.000000
+		mixrampdelay: nan
+		state: play
+		song: 146
+		songid: 3620
+		time: 0:231
+		elapsed: 0.000
+		bitrate: 32
+		audio: 48000:24:2
+		nextsong: 147
+		nextsongid: 3619
+		*/
+	})
 })
---
+++ notes.md
@@ -0,0 +1,42 @@
+# Notes
+
+## Database
+
+### Track
+
+- id
+- songid: same as mpd's `songid`
+- name: track's name
+
+### Beats
+
+- id
+- trackId
+- time: timecode where the beat is (given by aubio)
+
+## Behaviour
+
+### On playlist change
+
+Check if if song in the playlist is represented in the DB.
+Otherwise, run `aubiotrack` on each song and add the result in the DB.
+
+### On next/previous song, fast forward/backward, play
+
+Query the current time `time` or `elapsed` (what's the difference?) from the DB on `songid`.
+
+### On pause
+
+Interupt the current timer, so it doesn't trigger the next tick.
+
+## Thoughts
+
+1) To be efficient, query every tick for the current song and use `setTimeout` with
+the time difference between the next tick and the current one.
+
+1) Querying beats for the next song before its start will avoid some latency.
+
+1) The system may have some communication latency between MPD, this script and LEDs.
+Having the possiblity to add an offest to counterbalance this effect is probably a good idea.
+
+1) We still need some ideas to control the system.
